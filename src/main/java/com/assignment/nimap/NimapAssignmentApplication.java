6014e6da75e787368bf0fde902df661a324bf2dc
avinash aaglave <136832484+avinashaag96@users.noreply.github.com>
2023-08-13 15:41:06 +0530

    Add files via upload

---
+++ src/main/java/com/assignment/nimap/NimapAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.nimap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimapAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimapAssignmentApplication.class, args);
+	}
+
+}
